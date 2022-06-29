2146e4457df1dda85f60eadce341fb01e95d4186
Tommaso <tommaso.lodesani@gmail.com>
2022-06-29 17:29:06 +0200

    css

--- includes/main.php
+++ includes/main.php
@@ -1,17 +1,25 @@
-<main><?php
+<main>
+    <div class="container">
+        <div class="disc">
+    
+            <?php
+    
+            foreach ($arr_dischi as $disc) { ?>
+    
+                <img src="<?= $disc['poster']  ?>" alt="">
+                <div class="info">
+                    <h4><?= $disc['title'] ?></h4>
+                    <span><?= $disc['author'] ?></span>
+                    <span><?= $disc['year'] ?></span>
+    
+                </div><?php
+    
+            } ?>
+    
+    
+        </div>
 
-    foreach($arr_dischi as $disc){ ?>
 
-    <img src="<?=$disc['poster']  ?>" alt="">
-    <div>
-        <h4><?= $disc['title'] ?></h4>
-        <p><?= $disc['author'] ?></p>
-        <p><?= $disc['year'] ?></p> 
-
-    </div><?php
-
-    }?>
+    </div>
 
 </main>
-
-
--- indexprimo.php
+++ indexprimo.php
@@ -11,6 +11,7 @@ include __DIR__ . '/db/data.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body><?php
 
