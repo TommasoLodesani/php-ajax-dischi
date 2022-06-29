5ed5b5684f46d45ed2a3ee408bd1911467879e8b
Tommaso <tommaso.lodesani@gmail.com>
2022-06-29 17:49:40 +0200

    fix css

--- includes/main.php
+++ includes/main.php
@@ -1,23 +1,27 @@
 <main>
     <div class="container">
-        <div class="disc">
+        
     
             <?php
     
             foreach ($arr_dischi as $disc) { ?>
-    
-                <img src="<?= $disc['poster']  ?>" alt="">
-                <div class="info">
-                    <h4><?= $disc['title'] ?></h4>
-                    <span><?= $disc['author'] ?></span>
-                    <span><?= $disc['year'] ?></span>
-    
+
+                <div class="disc">
+
+                    <img src="<?= $disc['poster']  ?>" alt="">
+                    <div class="info">
+                        <h4><?= $disc['title'] ?></h4>
+                        <span><?= $disc['author'] ?></span>
+                        <span><?= $disc['year'] ?></span>
+                        
+                    </div>
+                        
                 </div><?php
     
             } ?>
     
     
-        </div>
+        
 
 
     </div>
